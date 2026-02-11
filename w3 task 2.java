973eb462f5f0c927b7f0f80645f1c70d743d30c8
vtu26741-web <vtu26741@veltech.edu.in>
2026-02-11 09:15:09 +0530

    Add files via upload

---
+++ w3 task 2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Task2 {
+
+    // Time: O(n * k), Space: O(1)
+    public static String getSmallestAndLargest(String s, int k) {
+
+        if (k > s.length() || k <= 0) {
+            return "Invalid k";
+        }
+
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+
+        for (int i = 1; i <= s.length() - k; i++) {
+            String sub = s.substring(i, i + k);
+
+            if (sub.compareTo(smallest) < 0) {
+                smallest = sub;
+            }
+            if (sub.compareTo(largest) > 0) {
+                largest = sub;
+            }
+        }
+
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
